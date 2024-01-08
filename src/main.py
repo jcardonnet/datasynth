ec99e31fe1213496212b950224affb4a37ba68ce
Julian Cardonnet <jcardonnet@gmail.com>
2024-01-08 15:19:46 -0300

    Add cache to GH workflow

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from pandas import DataFrame
 from pathlib import Path
 
 
-
 def hello() -> None:
     print("Hello World")
 
-def load_data(path: Path) -> DataFrame: 
+
+def load_data(path: Path) -> DataFrame:
     return pd.read_csv(path)
